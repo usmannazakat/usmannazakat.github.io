5865bcdc7b6f82e650af649b5941c7a4fb162e71
Usman Nazakat <162952388+usmannazakat@users.noreply.github.com>
2024-11-22 03:41:12 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -53,17 +53,50 @@ let interval = setInterval(() => {
 }, 500);
 
 
+const profileSec = document.querySelector('.profile');
 window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
-    if (window.scrollY > 70) 
+    if (window.scrollY > 100) 
     {
         header.classList.add("scrolled");
+        profileSec.classList.add('profile-scrolled');
     } 
     else 
     {
         header.classList.remove("scrolled");
+        profileSec.classList.remove('profile-scrolled');
     }
 });
 
 
+const animatedElements = document.querySelectorAll('h3, h4, h5, span, li, a, img, p');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        } else {
+            entry.target.classList.remove('animate');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+animatedElements.forEach(element => observer.observe(element));
+
+
+// Select the custom cursor elements
+const cursor = document.querySelector('.cursor');
+const cursorTrail = document.querySelector('.cursor-trail');
+
+// Update cursor position
+document.addEventListener('mousemove', (e) => {
+    // Set the main cursor position
+    cursor.style.left = `${e.clientX}px`;
+    cursor.style.top = `${e.clientY}px`;
+
+    // Set the trail cursor position with a slight delay
+    cursorTrail.style.left = `${e.clientX}px`;
+    cursorTrail.style.top = `${e.clientY}px`;
+});
 
